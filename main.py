a1042ba7dc40e844216915f4fd511a39087196ac
Reis Caffrey <rcaffrey@proton.me>
2024-12-08 14:44:54 +0100

    Added route in main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from flask import Flash, request, jsonify
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Home"
+
+if __name__ == "__main__":
+    app.run(debug=True)
